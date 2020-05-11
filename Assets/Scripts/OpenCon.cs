324c1c954aa1bd27b9e731d9ad7cc3b5f6b204d9
sanderoost <sander-oostenbrink@hotmail.com>
2020-05-11 11:02:05 +0200

    Memory showing

---
+++ Assets/Scripts/OpenCon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+using System.IO;
+
+public class OpenCon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Create database
+        string connection = "URI=file:" + Application.persistentDataPath + "/OudeKerk";
+
+        // open Connection
+        IDbConnection dbcon = new SqliteConnection(connection);
+        dbcon.Open();
+        IDataReader reader;
+
+        //// Create table
+        IDbCommand dbcmd = dbcon.CreateCommand();
+        string q_createTable =
+          "CREATE TABLE IF NOT EXISTS Memories (id INTEGER PRIMARY KEY, name TEXT, memory TEXT, date DATE )";
+
+        dbcmd.CommandText = q_createTable;
+        reader = dbcmd.ExecuteReader();
+
+        Debug.Log("We have a connection");
+
+        dbcon.Close();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
